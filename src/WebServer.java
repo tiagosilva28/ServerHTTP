629b4b3d148e8a771ebe13179a6b8b2bbc9f0cd7
tiaogosilva28 <tiagosilva28@live.com.pt>
2023-02-22 15:57:08 +0000

    GetRequest

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,6 +1,12 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import util.Messages;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -26,19 +32,42 @@ public class WebServer {
 
         // Initialize server socket and thread pool
         serverSocket = new ServerSocket(port);
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", port), 0);
+        server.createContext("/www", new RequestHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+        logger.info(" Server started on port 8001");
         service = Executors.newCachedThreadPool();
         Socket clientSocket = serverSocket.accept();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
     }
 
     private void serverRequests (ServerSocket serverSocket, ExecutorService service){
 
+
+
     }
 
-    private static class RequestHandler implements Runnable{
+    private static class RequestHandler implements HttpHandler {
 
         @Override
-        public void run() {
+        public void handle(HttpExchange exchange) throws IOException {
+            String request = null;
+            if ("GET".equals(exchange.getRequestMethod())){
+                request = handleGetRequest(exchange);
+                System.out.println(request);
+            }
 
+        }
+
+        private String handleGetRequest(HttpExchange httpExchange) {
+            return httpExchange.
+            getRequestURI()
+                    .toString()
+                    .split("\\?")[1]
+                    .split("=")[1];
         }
     }
 }
