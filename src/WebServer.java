a4dab2de7800d8b1df3f1273786b7abd8798cc2f
saratcb <saratcbrandao@gmail.com>
2023-02-22 16:41:50 +0000

    try response

--- src/WebServer.java
+++ src/WebServer.java
@@ -6,11 +6,16 @@ import util.Messages;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+
 
 public class WebServer {
     private ServerSocket serverSocket;
@@ -18,12 +23,12 @@ public class WebServer {
 
     public static void main(String[] args) throws IOException {
         int port = 8080;
-        if(System.getenv("PORT") !=null){
+        if (System.getenv("PORT") != null) {
             port = Integer.parseInt(System.getenv("PORT"));
         }
         try {
             new WebServer().start(port);
-        } catch (IOException e){
+        } catch (IOException e) {
             System.err.println(Messages.NO_SUCH_COMMAND);
         }
     }
@@ -31,20 +36,20 @@ public class WebServer {
     private void start(int port) throws IOException {
 
         // Initialize server socket and thread pool
-        serverSocket = new ServerSocket(port);
+        // serverSocket = new ServerSocket(port);
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", port), 0);
         server.createContext("/www", new RequestHandler());
-        server.setExecutor(threadPoolExecutor);
-        server.start();
-        logger.info(" Server started on port 8001");
         service = Executors.newCachedThreadPool();
-        Socket clientSocket = serverSocket.accept();
+        server.setExecutor(service);
+        server.start();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        //Socket clientSocket = serverSocket.accept();
+
+       // BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
     }
 
-    private void serverRequests (ServerSocket serverSocket, ExecutorService service){
+    private void serverRequests(ServerSocket serverSocket, ExecutorService service) {
     }
 
     private static class RequestHandler implements HttpHandler {
@@ -52,20 +57,32 @@ public class WebServer {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             String request = null;
-            if ("GET".equals(exchange.getRequestMethod())){
+            if ("GET".equals(exchange.getRequestMethod())) {
                 request = handleGetRequest(exchange);
                 System.out.println(request);
             }
-
+            handleResponse(exchange);
         }
+
         private String handleGetRequest(HttpExchange httpExchange) {
             return httpExchange.
-            getRequestURI()
+                    getRequestURI()
                     .toString()
                     .split("\\?")[1]
                     .split("=")[1];
         }
 
+        private void handleResponse(HttpExchange httpExchange) throws IOException {
 
+            OutputStream outputStream = httpExchange.getResponseBody();
+            String htmlResponse = String.valueOf(Paths.get("www/index.html"));
+
+            httpExchange.sendResponseHeaders(200, htmlResponse.length());
+
+            outputStream.write(htmlResponse.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+        }
     }
 }
